e0487e97628765f107ce84b0402cd57920cf2e64
DESKTOP-UUNOHSD\kaler <boudin.joey.pro@gmail.com>
2021-11-22 17:14:55 +0100

    Migration ajouté pour l'age minimum requis pour un evenement, et modification de Event_Adresse en EventAdresse

---
+++ portal_project/Migrations/202111221613151_AjoutAgeMinEvent.cs
@@ -0,0 +1,22 @@
+﻿namespace portal_project.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AjoutAgeMinEvent : DbMigration
+    {
+        public override void Up()
+        {
+            RenameColumn(table: "dbo.Event", name: "Event_Adresse_Id", newName: "EventAdresse_Id");
+            RenameIndex(table: "dbo.Event", name: "IX_Event_Adresse_Id", newName: "IX_EventAdresse_Id");
+            AddColumn("dbo.Event", "MinAge", c => c.Int(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Event", "MinAge");
+            RenameIndex(table: "dbo.Event", name: "IX_EventAdresse_Id", newName: "IX_Event_Adresse_Id");
+            RenameColumn(table: "dbo.Event", name: "EventAdresse_Id", newName: "Event_Adresse_Id");
+        }
+    }
+}
