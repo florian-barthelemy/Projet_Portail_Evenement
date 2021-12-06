34c3bc450ca6d87ae1b1483444f657a9692b287f
Jason <jasonpoquet@outlook.fr>
2021-12-06 10:49:52 +0100

    Ajout données Catégories et Sous-catégories

---
+++ portal_project/Migrations/202112060929494_Ajout_Type_Evenements.cs
@@ -0,0 +1,32 @@
+﻿namespace portal_project.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Ajout_Type_Evenements : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO Categorie(Libelle) VALUES('Culture')");
+            Sql("INSERT INTO Categorie(Libelle) VALUES('Sport')");
+
+            Sql("Insert INTO SousCategorie(Libelle,EventCategorie_Id) VALUES('Concert',1) ");
+            Sql("Insert INTO SousCategorie(Libelle,EventCategorie_Id) VALUES('Art',1) ");
+            Sql("Insert INTO SousCategorie(Libelle,EventCategorie_Id) VALUES('Histoire',1) ");
+            Sql("Insert INTO SousCategorie(Libelle,EventCategorie_Id) VALUES('Festival de musique',1) ");
+
+            Sql("Insert INTO SousCategorie(Libelle,EventCategorie_Id) VALUES('Football',2) ");
+            Sql("Insert INTO SousCategorie(Libelle,EventCategorie_Id) VALUES('Rugby',2) "); 
+            Sql("Insert INTO SousCategorie(Libelle,EventCategorie_Id) VALUES('Trail',2) ");
+            Sql("Insert INTO SousCategorie(Libelle,EventCategorie_Id) VALUES('Fitness',2) ");
+            Sql("Insert INTO SousCategorie(Libelle,EventCategorie_Id) VALUES('Judo',2) ");
+            Sql("Insert INTO SousCategorie(Libelle,EventCategorie_Id) VALUES('Karaté',2) ");
+        }
+        
+        public override void Down()
+        {
+            Sql("DELETE FROM Categorie");
+            Sql("DELETE FROM SousCategorie");
+        }
+    }
+}
