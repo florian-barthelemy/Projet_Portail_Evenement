4259729e94e195e4d54bbdd4fb623eaf628c8123
florian-barthelemy <florian-barthelemy@hotmail.com>
2021-12-06 12:01:22 +0100

    ajout CategorieService

--- TestUnitaire/CategorieTest.cs
+++ TestUnitaire/CategorieTest.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using portal_project.Models;
+using portal_project.Service;
 using System;
 using System.Collections.Generic;
 using TestUnitaire.Mock;
@@ -10,10 +11,12 @@ namespace TestUnitaire
     public class CategorieTest
     {
         CategorieMockDao dao;
+        CategorieService service;
         [TestInitialize] //Setup: s'execute avant chaque méthode de test
         public void Setup()
         {
             dao = new CategorieMockDao();
+            service = new CategorieService(dao);
             dao.Categories.Add(new Categorie(1, "Sportif"));
         }
 
@@ -28,7 +31,7 @@ namespace TestUnitaire
         [TestProperty("Test Categorie", "Create")]
         public void CreateCategorie_DifferentId_Return_Count_Equals2()
         {
-            dao.createCategorie(new Categorie(2, "Culturel"));
+            service.createCategorie(new Categorie(2, "Culturel"));
             Assert.AreEqual(2, dao.Categories.Count);
         }
 
@@ -37,7 +40,7 @@ namespace TestUnitaire
         [TestProperty("Test Categorie", "Create")]
         public void CreateCategorie_SameId_Return_Count_Equals1()
         {
-            dao.createCategorie(new Categorie(1, "Culturel"));
+            service.createCategorie(new Categorie(1, "Culturel"));
             Assert.AreEqual(1, dao.Categories.Count);
         }
 
@@ -46,7 +49,7 @@ namespace TestUnitaire
         [TestProperty("Test Categorie", "Delete")]
         public void DeleteCategorie_Id_Known_Return_Count_Equals0()
         {
-            dao.deleteCategorie(1);
+            service.deleteCategorie(1);
             Assert.AreEqual(0, dao.Categories.Count);
         }
 
@@ -55,7 +58,7 @@ namespace TestUnitaire
         [TestProperty("Test Categorie", "Delete")]
         public void DeleteCategorie_Id_Unknown_Return_Count_Equals1()
         {
-            dao.deleteCategorie(2);
+            service.deleteCategorie(2);
             Assert.AreEqual(1, dao.Categories.Count);
         }
 
@@ -65,8 +68,8 @@ namespace TestUnitaire
         public void EditCategorie_Id_Known_Return_Item_Edited()
         {
             Categorie cat = new Categorie(1, "Culturel");
-            dao.editCategorie(cat);
-            Assert.AreEqual(cat.Libelle, dao.findOneById(1).Libelle);
+            service.editCategorie(cat);
+            Assert.AreEqual(cat.Libelle, service.findOneById(1).Libelle);
         }
 
         [TestMethod]
@@ -75,8 +78,8 @@ namespace TestUnitaire
         public void EditCategorie_Id_Unknown_Return_Item_NotEdited()
         {
             Categorie cat = new Categorie(2, "Culturel");
-            dao.editCategorie(cat);
-            Assert.AreEqual("Sportif", dao.findOneById(1).Libelle);
+            service.editCategorie(cat);
+            Assert.AreEqual("Sportif", service.findOneById(1).Libelle);
         }
 
         [TestMethod]
@@ -84,7 +87,7 @@ namespace TestUnitaire
         [TestProperty("Test Categorie", "FindById")]
         public void FindById_UnknowId_Return_Object_Null()
         {
-            Categorie cat = dao.findOneById(2);
+            Categorie cat = service.findOneById(2);
             Assert.AreEqual(null, cat);
         }
 
@@ -93,7 +96,7 @@ namespace TestUnitaire
         [TestProperty("Test Categorie", "FindById")]
         public void FindById_knowId_Return_Libelle_EqualsSportif()
         {
-            Categorie cat = dao.findOneById(1);
+            Categorie cat = service.findOneById(1);
             Assert.AreEqual("Sportif", cat.Libelle);
         }
 
@@ -103,7 +106,7 @@ namespace TestUnitaire
         [TestProperty("Test Categorie", "FindAll")]
         public void FindAll_Categorie_Return_Count_Equals1()
         {
-            List<Categorie> categories = dao.getAllCategories();
+            List<Categorie> categories = service.getAllCategories();
             Assert.AreEqual(1, categories.Count);
         }
 
---
+++ portal_project/Service/CategorieService.cs
@@ -0,0 +1,46 @@
+﻿using portal_project.Metier;
+using portal_project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace portal_project.Service
+{
+   public class CategorieService
+    {
+        private ICategorie dao;
+
+        public CategorieService(ICategorie dao)
+        {
+            this.dao = dao;
+        }
+
+        public void createCategorie(Categorie categorie)
+        {
+            dao.createCategorie(categorie);
+        }
+
+        public void editCategorie(Categorie categorie)
+        {
+            dao.editCategorie(categorie);
+        }
+
+        public void deleteCategorie(int id_categorie)
+        {
+            dao.deleteCategorie(id_categorie);
+        }
+
+        public List<Categorie> getAllCategories()
+        {
+            return dao.getAllCategories();
+        }
+
+        public Categorie findOneById(int id)
+        {
+            return dao.findOneById(id);
+        }
+
+    }
+}
