acf3347b09d4e76520349a0517f7239f295516c3
florian-barthelemy <florian-barthelemy@hotmail.com>
2021-12-06 12:19:22 +0100

    ajout SousCategorieService et modif SousCategorieTest

--- TestUnitaire/SousCategorieTest.cs
+++ TestUnitaire/SousCategorieTest.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using portal_project.Models;
+using portal_project.Service;
 using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
@@ -11,10 +12,12 @@ namespace TestUnitaire
     public class SousCategorieTest
     {
         SousCategorieMockDao dao;
+        SousCategorieService service;
         [TestInitialize] //Setup: s'execute avant chaque méthode de test
         public void Setup()
         {
             dao = new SousCategorieMockDao();
+            service = new SousCategorieService(dao);
             Categorie categorie = new Categorie {Id= 1, Libelle="Sportif"};
             SousCategorie sousCategorie = new SousCategorie {EventCategorie=categorie, Id=1, Libelle="Foot" };
             dao.SousCategories.Add(sousCategorie);
@@ -31,7 +34,7 @@ namespace TestUnitaire
         public void CreateSousCategorie_DifferentId_Return_Count_Equals2()
         {
             SousCategorie sousCategorie = new SousCategorie { Id = 2, Libelle = "Rugby" };
-            dao.createSousCategorie(sousCategorie);
+            service.createSousCategorie(sousCategorie);
             Assert.AreEqual(2, dao.SousCategories.Count);
         }
 
@@ -41,7 +44,7 @@ namespace TestUnitaire
         public void CreateSousCategorie_SameId_Return_Count_Equals1()
         {
             SousCategorie sousCategorie = new SousCategorie { Id = 1, Libelle = "Rugby" };
-            dao.createSousCategorie(sousCategorie);
+            service.createSousCategorie(sousCategorie);
             Assert.AreEqual(1, dao.SousCategories.Count);
         }
 
@@ -51,7 +54,7 @@ namespace TestUnitaire
         public void DeleteSousCategorie_Id_Known_Return_Count_Equals0()
         {
 
-            dao.deleteSousCategorie(1);
+            service.deleteSousCategorie(1);
             Assert.AreEqual(0, dao.SousCategories.Count);
         }
         [TestMethod]
@@ -60,7 +63,7 @@ namespace TestUnitaire
         public void DeleteSousCategorie_Id_Unknown_Return_Count_Equals1()
         {
 
-            dao.deleteSousCategorie(2);
+            service.deleteSousCategorie(2);
             Assert.AreEqual(1, dao.SousCategories.Count);
         }
 
@@ -70,8 +73,8 @@ namespace TestUnitaire
         public void EditSousCategorie_Id_Known_Return_Item_Edited()
         {
             SousCategorie sousCategorie = new SousCategorie { Id = 1, Libelle = "Rugby" };
-            dao.editSousCategorie(sousCategorie);
-            Assert.AreEqual(sousCategorie.Libelle, dao.findOneById(1).Libelle);
+            service.editSousCategorie(sousCategorie);
+            Assert.AreEqual(sousCategorie.Libelle, service.findOneById(1).Libelle);
         }
 
         [TestMethod]
@@ -80,8 +83,8 @@ namespace TestUnitaire
         public void EditSousCategorie_Id_Unknown_Return_Item_Edited()
         {
             SousCategorie sousCategorie = new SousCategorie { Id = 2, Libelle = "Rugby" };
-            dao.editSousCategorie(sousCategorie);
-            Assert.AreEqual("Foot", dao.findOneById(1).Libelle);
+            service.editSousCategorie(sousCategorie);
+            Assert.AreEqual("Foot", service.findOneById(1).Libelle);
         }
 
         [TestMethod]
@@ -89,7 +92,7 @@ namespace TestUnitaire
         [TestProperty("Test SousCategorie", "FindOneById")]
         public void FindByIdSousCategorie_Id_Known_Return_()
         {
-            SousCategorie sousCategorie = dao.findOneById(1);
+            SousCategorie sousCategorie = service.findOneById(1);
             Assert.AreEqual("Foot", sousCategorie.Libelle);
         }
 
@@ -98,7 +101,7 @@ namespace TestUnitaire
         [TestProperty("Test SousCategorie", "FindOneById")]
         public void FindByIdSousCategorie_Id_Unknown_Return_()
         {
-            SousCategorie sousCategorie = dao.findOneById(2);
+            SousCategorie sousCategorie = service.findOneById(2);
             Assert.AreEqual(null, sousCategorie);
         }
 
@@ -107,7 +110,7 @@ namespace TestUnitaire
         [TestProperty("Test SousCategorie", "GetAll")]
         public void GetAllSousCategorie_Return_Count_Equals1()
         {
-            List<SousCategorie> sousCategories = dao.getAllSousCategories();
+            List<SousCategorie> sousCategories = service.getAllSousCategories();
             Assert.AreEqual(1, sousCategories.Count);
         }
 
@@ -116,7 +119,7 @@ namespace TestUnitaire
         [TestProperty("Test SousCategorie", "GetAllByCategorie")]
         public void GetAllByCategorieSousCategorie_Sportif_Return_Count_Equals1()
         {
-            List<SousCategorie> sousCategories = dao.getAllSousCategoriesbyCategorie("Sportif");
+            List<SousCategorie> sousCategories = service.getAllSousCategoriesbyCategorie("Sportif");
             Assert.AreEqual(1, sousCategories.Count);
         }
 
@@ -125,7 +128,7 @@ namespace TestUnitaire
         [TestProperty("Test SousCategorie", "GetAllByCategorie")]
         public void GetAllByCategorieSousCategorie_Culturel_Return_Count_Equals0()
         {
-            List<SousCategorie> sousCategories = dao.getAllSousCategoriesbyCategorie("Culturel");
+            List<SousCategorie> sousCategories = service.getAllSousCategoriesbyCategorie("Culturel");
             Assert.AreEqual(0, sousCategories.Count);
         }
 
---
+++ portal_project/Service/SousCategorieService.cs
@@ -0,0 +1,50 @@
+﻿using portal_project.Metier;
+using portal_project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace portal_project.Service
+{
+    public class SousCategorieService
+    {
+        private ISousCategorie dao;
+
+        public SousCategorieService(ISousCategorie dao)
+        {
+            this.dao = dao;
+        }
+
+        public void createSousCategorie(SousCategorie sousCategorie)
+        {
+            dao.createSousCategorie(sousCategorie);
+        }
+
+        public void editSousCategorie(SousCategorie sousCategorie)
+        {
+            dao.editSousCategorie(sousCategorie);
+        }
+
+        public void deleteSousCategorie(int id_categorie)
+        {
+            dao.deleteSousCategorie(id_categorie);
+        }
+
+        public List<SousCategorie> getAllSousCategories()
+        {
+            return dao.getAllSousCategories();
+        }
+
+        public List<SousCategorie> getAllSousCategoriesbyCategorie(string titre)
+        {
+            return dao.getAllSousCategoriesbyCategorie(titre);
+        }
+
+        public SousCategorie findOneById(int id)
+        {
+            return dao.findOneById(id);
+        }
+    }
+}
