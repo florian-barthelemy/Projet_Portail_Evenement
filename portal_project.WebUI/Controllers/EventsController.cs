f8e0a06f65003dcec1e0a7921db727c268b7e8a5
Jason <jasonpoquet@outlook.fr>
2021-12-16 10:30:24 +0100

    Ajout sideBar Dynamique

--- portal_project.WebUI/Controllers/EventsController.cs
+++ portal_project.WebUI/Controllers/EventsController.cs
@@ -1,6 +1,7 @@
 ﻿using portal_project.Dao;
 using portal_project.Models;
 using portal_project.Service;
+using portal_project.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,19 +12,37 @@ namespace portal_project.WebUI.Controllers
 {
     public class EventsController : Controller
     {
-        private CategorieService service;
-        private CategorieImpl dao;
+        private CategorieService catService;
+        private SousCategorieService souCatService;
+        private SousCategorieImpl sousCatDao;
+        private CategorieImpl catDao;
 
         public EventsController()
         {
-            dao = new CategorieImpl();
-            service = new CategorieService(dao);
+            catDao = new CategorieImpl();
+            catService = new CategorieService(catDao);
+            sousCatDao = new SousCategorieImpl();
+            souCatService = new SousCategorieService(sousCatDao);
         }
         // GET: Events
         public ActionResult Index()
         {
-            List<Categorie> cats = service.getAllCategories().ToList();
-            return View(cats);
+            
+            List<CategoryViewModel> vm = new List<CategoryViewModel>();
+            foreach (Categorie category in catDao.getAllCategories())
+            {
+                List<SousCategorie> souscats = new List<SousCategorie>();
+                foreach (SousCategorie souscat in sousCatDao.getAllSousCategoriesbyCategorie(category.Libelle))
+                    {
+                    souscats.Add(souscat);
+                    }
+                vm.Add(new CategoryViewModel { Categorie = category, LstSousCategories = souscats });
+
+            }
+            
+            //List<SousCategorie> sousCatsLst = souCatService.getAllSousCategories().ToList();
+            //List<SelectListItem> sousCats = new List<SelectListItem>() { };
+            return View(vm);
         }
     }
 }
---
+++ portal_project/ViewModels/CategoryViewModel.cs
@@ -0,0 +1,16 @@
+﻿using portal_project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace portal_project.ViewModels
+{
+    public class CategoryViewModel
+    {
+        public Categorie Categorie { get; set; }
+        public IEnumerable<SousCategorie> LstSousCategories { get; set; }
+
+    }
+}
