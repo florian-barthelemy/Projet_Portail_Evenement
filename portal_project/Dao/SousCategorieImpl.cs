be0fe6f92ab12e01ff4a034b41ae7e90ab8aabf4
florian-barthelemy <florian-barthelemy@hotmail.com>
2021-12-17 10:13:49 +0100

    Gestion des exception sous categories et suppresion des controles dans les implémentations

--- portal_project/Dao/SousCategorieImpl.cs
+++ portal_project/Dao/SousCategorieImpl.cs
@@ -15,34 +15,23 @@ namespace portal_project.Dao
 
         public void createSousCategorie(SousCategorie sousCategorie)
         {
-            SousCategorie newCat = context.SousCategories.SingleOrDefault(e => e.Id == sousCategorie.Id);
-            if (newCat == null)
-            {
-                context.SousCategories.Add(newCat);
+                context.SousCategories.Add(sousCategorie);
                 context.SaveChanges();
-            }
         }
 
 
         public void deleteSousCategorie(int id_sousCategorie)
         {
             SousCategorie deletedCat = context.SousCategories.SingleOrDefault(e => e.Id == id_sousCategorie);
-            if (deletedCat != null)
-            {
                 context.SousCategories.Remove(deletedCat);
                 context.SaveChanges();
-            }
         }
 
         public void editSousCategorie(SousCategorie sousCategorie)
         {
-            SousCategorie dbCategorie = context.SousCategories.SingleOrDefault(u => u.Id == sousCategorie.Id);
-            if (dbCategorie != null)
-            {
                 context.SousCategories.Attach(sousCategorie);
                 context.Entry(sousCategorie).State = EntityState.Modified;
                 context.SaveChanges();
-            }
         }
 
         public SousCategorie findOneById(int id)
