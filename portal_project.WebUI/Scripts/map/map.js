ee41f0dab722d4be90291d08c99663b0e049bd51
DESKTOP-UUNOHSD\kaler <boudin.joey.pro@gmail.com>
2021-12-23 17:26:43 +0100

    Split map JS et ajout champs recherche adresse

--- portal_project.WebUI/Scripts/map/map.js
+++ portal_project.WebUI/Scripts/map/map.js
@@ -1,68 +1,25 @@
-﻿
-$(document).ready(function () {
+﻿//création carte
+function createMap()
+{
     if ($('#map').length) {
 
-    
-    var map = L.map('map').setView([50.636565, 3.063528], 13);
-    $("#map").css('min-height', '500px');
 
-    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 18,
-        id: 'mapbox/streets-v11',
-        tileSize: 512,
-        zoomOffset: -1,
-        accessToken: 'pk.eyJ1Ijoia2FsZXJhc2VyIiwiYSI6ImNreGdhODVtazJnc3Iyb28xNHhuYzJkdmoifQ.I0vusjH3phNxs5E9VwJI0w'
-    }).addTo(map);
+        var map = L.map('map').setView([50.636565, 3.063528], 13);
+        $("#map").css('min-height', '500px');
 
-    $('#EventAdresse_Voie').keyup(function (e) {
-        e.preventDefault();
-        let adrLength = $(this).val().length;
-        if (adrLength > 1) {
-            let dataVal = $(this).val();
-            let url = "https://api-adresse.data.gouv.fr/search/?q=" + dataVal;
-
-
-
-            $.get(url,
-                function (data) {
-                    let tags = new Array();
-                    if (data.features.length < 5) {
-                        console.log(data.features);
-                        $('#listAdresses').empty();
-                        for (var i = 0; i < data.features.length; i++) {
-                            tags[i] = data.features[i].properties.label;
-                            $('#listAdresses').append("<option>" + tags[i]+"</option>");
-                        }
-                        if (data.features.length == 1) {
-                            map.eachLayer((layer) => {
-                                if (layer['_latlng'] != undefined)
-                                    layer.remove();
-                            });
-                            //récupération long lat 
-                            let lati = data.features[0].geometry.coordinates[1]; //y
-                            let longi = data.features[0].geometry.coordinates[0]; //x
-                            let ville = data.features[0].properties.city;
-                            let cp = data.features[0].properties.postcode;
-                            map.setView([lati, longi], 18);
-                            L.marker([lati, longi]).addTo(map); //ajout marker
-                            //longi = longi.replace(".", ",");
-                            //lati = lati.replace(".", ",");
-                            //insertion valeur Axe_X et Axe_y
-
-                            $('#EventAdresse_Axe_X').val(parseFloat(longi));
-                            $('#EventAdresse_Axe_Y').val(parseFloat(lati));
-
-                            //insertion Ville
-                            $('#EventAdresse_Ville').val(ville);
-                            //insertion code postal
-                            $('#EventAdresse_CodePostal').val(cp);
-                        }
-                        
-                    }
-                });
-        }
-
-    });
+        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: 'mapbox/streets-v11',
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken: 'pk.eyJ1Ijoia2FsZXJhc2VyIiwiYSI6ImNreGdhODVtazJnc3Iyb28xNHhuYzJkdmoifQ.I0vusjH3phNxs5E9VwJI0w'
+        }).addTo(map);
+        return map;
     } //endif map
-    });
+    else {
+        alert('erreur js');
+        return null;
+    }
+}
+
---
+++ portal_project.WebUI/Scripts/map/mapCreateEditEvent.js
@@ -0,0 +1,59 @@
+﻿//manipualation carte Event Create/Edit
+$(document).ready(function () {
+    var map = createMap();
+    if ($("#map").length) {
+        $('#searchAdresse').keyup(function (e) {
+            e.preventDefault();
+            let adrLength = $(this).val().length;
+            if (adrLength > 1) {
+                let dataVal = $(this).val();
+                let url = "https://api-adresse.data.gouv.fr/search/?q=" + dataVal;
+
+
+
+                $.get(url,
+                    function (data) {
+                        let tags = new Array();
+                        if (data.features.length < 5) {
+                            console.log(data.features);
+                            $('#listAdresses').empty();
+                            for (var i = 0; i < data.features.length; i++) {
+                                tags[i] = data.features[i].properties.label;
+                                $('#listAdresses').append("<option>" + tags[i] + "</option>");
+                            }
+                            if (data.features.length == 1) {
+                                map.eachLayer((layer) => {
+                                    if (layer['_latlng'] != undefined)
+                                        layer.remove();
+                                });
+                                //récupération long lat 
+                                let lati = data.features[0].geometry.coordinates[1]; //y
+                                let longi = data.features[0].geometry.coordinates[0]; //x
+                                let road = data.features[0].properties.name;
+                                let ville = data.features[0].properties.city;
+                                let cp = data.features[0].properties.postcode;
+                                map.setView([lati, longi], 18);
+                                L.marker([lati, longi]).addTo(map); //ajout marker
+                                //longi = longi.replace(".", ",");
+                                //lati = lati.replace(".", ",");
+                                //insertion valeur Axe_X et Axe_y
+
+                                $('#EventAdresse_Axe_X').val(parseFloat(longi));
+                                $('#EventAdresse_Axe_Y').val(parseFloat(lati));
+
+                                //isertion rue/voie
+                                $('#EventAdresse_Voie').val(road);
+                                //insertion Ville
+                                $('#EventAdresse_Ville').val(ville);
+                                //insertion code postal
+                                $('#EventAdresse_CodePostal').val(cp);
+                            }
+
+                        }
+                    });
+            }
+
+        });
+    } //endif map
+
+}); //end ready
